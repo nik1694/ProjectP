684ab7478f8733a16cd0f957a1a95314f7fd9c42
Nikhil <amrutkarnikhil29@gmail.com>
2018-07-10 11:29:14 +0530

    changes in title of website

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>My Website</title>
+    <title>Saundrya Palour</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
