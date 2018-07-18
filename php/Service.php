c79d67403391d1fcfd6c120125dbd00a09a6a05b
Nikhil <amrutkarnikhil29@gmail.com>
2018-07-18 13:33:27 +0530

    Adds files

---
+++ php/Service.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Services - Papillonsalons</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="no-bg">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="index.html" class="navbar-brand"><img src="images/inner-logo.png" /></a>
+            </div>
+            <div class="navbar-collapse collapse" id="navbar">
+                
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">Home</a></li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown disable" class="dropdown-toggle" href="#">About Us<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="about-papillon.html">About Papillon</a></li>
+                            <li><a href="dr-vinay-koparkar.html">Dr. Vinay Koparkar</a></li>
+                            <li><a href="kavita_koparkar.html">Kavita Koparkar</a></li>
+                            <li><a href="press_releases.html">Press Releases</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown disable" class="dropdown-toggle" href="#">Branches<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="ladies-beauty-parlour-kothrud-pune.html">Ladies Beauty Parlor</a></li>
+                            <li><a href="kavita-koparkar-studio-pune.html">KK Studio</a></li>
+                            <li><a href="hair-face-clinic-pune.html">Hair & Face Clinic</a></li>
+                            <li><a href="wigs_and_weaves.html">Papillon Wigs & Weaves</a></li>
+                        </ul>
+                    </li>
+                    <li class="active"><a href="ladies-beauty-parlour-services-kothrud-pune.html">Services</a></li>
+                    <li><a href="work_gallery.html">Work Gallery</a></li>
+                    <li><a href="courses.html">Courses</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+    <div class="banner8"></div>
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center">Services</h1>
+                </div>
+            </div>
+            <p> Services we offer for women</p>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="services">Haircut</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Hair Colour (Dye,  global colour)</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Highlighting</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Straightening</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="services">Rebonding</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Perming</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Hair Setting (Blow dry setting, Ironing, Curling, Roler Setting etc.)</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Threading</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="services">Facial (Herbal,  Fruit, Gold, Pearl etc.)</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Scrub</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Bleach</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Head Massage</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="services">Mehendi  Application</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Deep  Conditioning</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Waxing</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Manicure</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="services">Pedicure</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="services">Bridal Makeup</div>
+                </div>
+                
+            </div>
+            
+
+        </div>
+    </section>
+    <section class="branches">
+        <div class="container">
+            <div class="text-center">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="text-center">Our Branches</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                        <a href="ladies-beauty-parlour-kothrud-pune.html">
+                            <div class="image"><img src="images/profile1.jpg" /></div>
+                            <div class="pro-line">Ladies Beauty Parlour</div>
+                        </a>
+                        <ul>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="ladies-beauty-parlour-kothrud-pune.html">Haircut / Hairstyle</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="ladies-beauty-parlour-kothrud-pune.html">Facial / Scrub / Bleach</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="ladies-beauty-parlour-kothrud-pune.html">Waxing / Manicure / Pedicure</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="ladies-beauty-parlour-kothrud-pune.html">Bridal Makeup</a></li>
+                        </ul>
+                        <div class="visit"><a href="ladies-beauty-parlour-kothrud-pune.html" target="_blank">Visit Page</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <a href="kkstudio.html">
+                            <div class="image"><img src="images/profile2.jpg" /></div>
+                            <div class="pro-line">KK Studio</div>
+
+                        </a>
+                        <ul>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.kavitakoparkar.com/about-kavita-koparkar.html" target="_blank">About Kavita Koparkar</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.kavitakoparkar.com/kkstudio.html" target="_blank">About KK Studio</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.kavitakoparkar.com/bridal-packages.html" target="_blank">Bridal Packages</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.kavitakoparkar.com/courses-offered.html" target="_blank">The KK Academy</a></li>
+                        </ul>
+                        <div class="visit"><a href="http://www.kavitakoparkar.com/" target="_blank">Visit Website</a></div>
+                    </div>
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="col-sm-6 col-md-3">
+                        <a href="hair-face-clinic-pune.html">
+                            <div class="image"><img src="images/profile3.jpg" /></div>
+                            <div class="pro-line">Hair & Face Clinic</div>
+
+                        </a>
+                        <ul>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="hair-face-clinic-pune.html">Hair Care Services</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="hair-face-clinic-pune.html">Hair Loss Treatments</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="hair-face-clinic-pune.html">Pimples / Acne Treatments</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="hair-face-clinic-pune.html">Pigmentation Treatments</a></li>
+                        </ul>
+                        <div class="visit"><a href="hair-face-clinic-pune.html" target="_blank">Visit Page</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <a href="wigs_and_weaves.html">
+                            <div class="image"><img src="images/profile4.jpg" /></div>
+                            <div class="pro-line">Papillon Wigs and Weaves</div>
+
+                        </a>
+                        <ul>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.papillonwigsandweaves.com/hair-loss-treatment.html" target="_blank">Hair Loss / Hair Care</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.papillonwigsandweaves.com/hair_weaving_bonding_for_men.html" target="_blank">Non-Surgical Hair Replacement</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.papillonwigsandweaves.com/hair-transplant-for-men.html" target="_blank">Hair Transplant</a></li>
+                            <li><span><i class="fa fa-check-square-o"></i></span><a href="http://www.papillonwigsandweaves.com/hair_wigs_for_chemotherapy_patients.html" target="_blank">Hair Wigs</a></li>
+                        </ul>
+                        <div class="visit"><a href="http://papillonwigsandweaves.com/" target="_blank">Visit Website</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="sm-logo"><a href="index.html"><img src="images/small-logo.png" /></a></div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="text-center mrg-top">Papillonsalons &copy; 2016. <a href="http://www.on-click.in/services/design/website-designing.html" target="_blank">Website Designing</a> by <a href="http://www.on-click.in/" target="_blank">Onclick</a>.</div>
+                </div>
+                <div class="col-sm-3">
+                    <ul class="social">
+                        <li><a href="https://www.facebook.com/KavitaKoparkarBridalMakeup" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/lightbox.min.js"></script>
+    <script>
+        $(".branches ul li").hover(function () {
+            $(this).find("a").addClass("animated pulse");
+        }, function () {
+            $(this).find("a").removeClass("animated pulse");
+        });
+    </script>
+</body>
+</html>
